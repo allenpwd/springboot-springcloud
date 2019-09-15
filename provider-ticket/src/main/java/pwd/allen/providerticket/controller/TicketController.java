4dcbd7f8c594815bb516775c602d70ba4b3e9dc5
门那粒沙 <994266136@qq.com>
2019-09-15 23:30:46 +0800

    Initial commit

---
+++ provider-ticket/src/main/java/pwd/allen/providerticket/controller/TicketController.java
@@ -0,0 +1,19 @@
+package pwd.allen.providerticket.controller;
+
+import pwd.allen.providerticket.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TicketController {
+
+
+    @Autowired
+    TicketService ticketService;
+
+    @GetMapping("/ticket")
+    public String getTicket(){
+        return ticketService.getTicket();
+    }
+}
